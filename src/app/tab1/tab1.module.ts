5d7f9dcbf69c07a94cd9254bec782965c05a0e99
Julio Graterol <juliograterolb@gmail.com>
2023-11-05 16:16:24 -0400

    added tweet component

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { TweetComponent } from '../tweet/tweet.component';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -13,8 +14,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, TweetComponent],
 })
 export class Tab1PageModule {}
