5eae13d0108717815077faa9d224a98c5e22af73
Julio Graterol <juliograterolb@gmail.com>
2023-11-06 11:32:30 -0400

    added Alerts and fixed styles

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import FetchApi from '../services/fetchapi.service';
+import { AlertController } from '@ionic/angular';
+
 // import { Router } from '@angular/router';
 
 @Component({
@@ -9,10 +11,14 @@ import FetchApi from '../services/fetchapi.service';
 })
 export class LoginPage {
   userData = {
-    email: '',
+    identifier: '',
     password: '',
   };
-  constructor(private apiService: FetchApi) {}
+  constructor(
+    private apiService: FetchApi,
+    private alertController: AlertController,
+    private renderer: Renderer2
+  ) {}
 
   async login() {
     // Aquí deberías agregar la lógica de inicio de sesión
@@ -21,19 +27,46 @@ export class LoginPage {
     try {
       // Define los datos que deseas enviar en el cuerpo de la solicitud
 
+      if (!this.userData.identifier || !this.userData.password) {
+        this.presentAlert(
+          'Datos Incompeltos',
+          'Para iniciar sesión debe llenar todo los datos',
+          'OK'
+        );
+        return;
+      }
+
       const response = await this.apiService.request(
         'POST',
         {
-          identifier: this.userData.email,
+          identifier: this.userData.identifier,
           password: this.userData.password,
         },
-        'http://localhost:3000/auth/login'
+        `http://localhost:3000/auth/login`
       );
+
+      if (!response) {
+        this.presentAlert(
+          'Datos Erroneos',
+          'El usuario/correo, o la contraseña no son correctos',
+          'OK'
+        );
+      }
 
       console.log('Respuesta del servidor:', response);
     } catch (error) {
       console.error('Error al realizar la solicitud:', error);
     }
     // Inicio de sesión fallido, muestra un mensaje de error
+  }
+  async presentAlert(header: string, message: string, btnText: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      // subHeader: 'Important message',
+      message: message,
+      buttons: [btnText],
+    });
+
+    await alert.present();
   }
 }
