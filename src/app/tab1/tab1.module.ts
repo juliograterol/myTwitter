7df1778ca331015daa63ffe288c0e6441b3b9630
Julio Graterol <juliograterolb@gmail.com>
2023-11-06 20:02:13 -0400

    added tweets to component folder

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-import { TweetComponent } from '../tweet/tweet.component';
+import { TweetComponent } from '../components/tweet/tweet.component';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
